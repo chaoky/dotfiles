8db94338db9dc53d1e7b729e37772136b01adf22
lordie <levimanga@gmail.com>
2019-09-26 20:44:23 -0300

    a

--- vscode-oss/extensions/alanz.vscode-hie-server-0.0.28/out/src/commands/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CommandNames;
-(function (CommandNames) {
-    CommandNames.InsertTypeCommandName = 'hie.commands.insertType';
-    CommandNames.ShowTypeCommandName = 'hie.commands.showType';
-    CommandNames.ImportIdentifierCommandName = 'hie.commands.importIdentifier';
-})(CommandNames = exports.CommandNames || (exports.CommandNames = {}));
-//# sourceMappingURL=constants.js.map
