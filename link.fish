9cab54c9571ca127f7f80c45e8953725177e09b8
lordie <levimanga@gmail.com>
2021-02-26 14:10:17 -0300

    copy old scripts

--- link.fish
+++ link.fish
@@ -4,3 +4,8 @@ for file in */*
     rm -r $HOME/.(string replace home/ '' $file)
     ln -sv (realpath $file) $HOME/.(string replace home/ '' $file)
 end
+
+chmod 600 $HOME/.ssh/id_rsa
+# antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+git config --global user.email "levimanga@gmail.com" && git config --global user.name "lordie"
+git remote set-url origin git@github.com:chaoky/dotfiles.git
