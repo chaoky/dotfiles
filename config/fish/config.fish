6afac587524967bb0a68a0cec4d926d65b6aa427
lordie <levimanga@gmail.com>
2022-05-21 19:10:42 -0300

    emacs vterm in fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,15 +1,14 @@
 set fish_greeting
-# bass source ~/.profile
 
-# setxkbmap -model pc104 -layout us -variant colemak
+# alias kc '/usr/bin/keychain -q --nogui $HOME/.ssh/id_rsa && source $HOME/.keychain/stanbot-15-fish'
 
-alias tablet '/home/lordie/Android/Sdk/emulator/emulator @tablet'
 alias ls 'ls --color=auto'
-alias docker 'sudo docker'
 alias yk 'xsel --clipboard --input'
 alias pp 'xsel --clipboard --output'
 
-alias kc '/usr/bin/keychain -q --nogui $HOME/.ssh/id_rsa && source $HOME/.keychain/stanbot-15-fish'
-
 starship init fish | source
 direnv hook fish | source
+
+if [ "$INSIDE_EMACS" = vterm ]
+    source $EMACS_VTERM_PATH/etc/emacs-vterm.fish
+end
