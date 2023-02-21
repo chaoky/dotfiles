68ab1d9677f5d1a53e79f691e923024edaee7bcc
lordie <levimanga@gmail.com>
2023-02-21 17:24:19 -0300

    update link.fish remove symliks before recreating

--- link.fish
+++ link.fish
@@ -1,11 +1,14 @@
 #!/usr/bin/env fish
 
+for file in (find $HOME -maxdepth 2 -lname '*dotfiles*')
+    unlink $file
+end
+
 for file in config/* home/*
-    rm -ri $HOME/.(string replace home/ '' $file)
-    ln -sv (realpath $file) $HOME/.(string replace home/ '' $file)
+    ln -svb (realpath $file) $HOME/.(string replace home/ '' $file)
 end
 
 chmod 600 $HOME/.ssh/id_rsa
 # antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
-git config --global user.email "levimanga@gmail.com" && git config --global user.name "lordie"
+git config --global user.email "levimanga@gmail.com" && git config --global user.name lordie
 git remote set-url origin git@github.com:chaoky/dotfiles.git
