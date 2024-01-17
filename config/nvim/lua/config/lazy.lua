5ff8dfd8077b586b062b16c06f768fcd6fbca9e0
chaoky <levimanga@gmail.com>
2024-01-17 15:26:41 -0300

    update nvim pkgs

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -55,7 +55,7 @@ require("lazy").setup({
         timeout = 300,
       },
       keys = {
-        { "ii", "<Esc>", mode = "i" },
+        { "ii", "<Esc>",       mode = "i" },
         { "ii", "<C-\\><C-n>", mode = "t" },
       },
     },
@@ -130,13 +130,13 @@ require("lazy").setup({
       "folke/which-key.nvim",
       opts = {
         presets = {
-          operators = false, -- adds help for operators like d, y, ...
-          motions = false, -- adds help for motions
+          operators = false,    -- adds help for operators like d, y, ...
+          motions = false,      -- adds help for motions
           text_objects = false, -- help for text objects triggered after entering an operator
-          windows = false, -- default bindings on <c-w>
-          nav = false, -- misc bindings to work with windows
-          z = true, -- bindings for folds, spelling and others prefixed with z
-          g = true, -- bindings for prefixed with g
+          windows = false,      -- default bindings on <c-w>
+          nav = false,          -- misc bindings to work with windows
+          z = true,             -- bindings for folds, spelling and others prefixed with z
+          g = true,             -- bindings for prefixed with g
         },
         triggers_blacklist = {
           i = { "i" },
