85830bc5dbebade6c4eef6cf4a1b09e0d167494e
chaoky <levimanga@gmail.com>
2024-03-07 07:58:13 -0300

    self-elevate wsl_foward.ps1

--- wsl_foward.ps1
+++ wsl_foward.ps1
@@ -1,3 +1,12 @@
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+ if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  Exit
+ }
+}
+
 $remoteport = bash.exe -c "ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'"
 $found = $remoteport -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
 
@@ -11,7 +20,7 @@ if( $found ){
 #[Ports]
 
 #All the ports you want to forward separated by coma
-$ports=@(1234, 3000, 3001,8081, 8080, 8082, 8083, 54321, 54322, 54323, 54324);
+$ports=@(1234, 3000, 3001, 8000, 8081, 8080, 8082, 8083, 54321, 54322, 54323, 54324);
 
 
 #[Static ip]
