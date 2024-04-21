ff63441b3e7220e94e6dbaed029343e8fab14daf
chaoky <levimanga@gmail.com>
2024-04-21 18:51:54 -0300

    update fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -17,7 +17,8 @@ set BUN_INSTALL ~/.bun
 fish_add_path ~/.bun/bin
 
 set fish_greeting
-set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
+set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh
+set -gx PAGER cat
 # set -x DISPLAY :1.0
 # set -x GDK_BACKEND x11
 
@@ -26,5 +27,6 @@ alias dps 'docker ps --format "table{{.Names}}\t{{.Status}}\t{{.Ports}}"'
 alias dbr 'docker run --rm -it (docker build -q .)'
 #alias hm 'home-manager switch --flake ~/dotfiles/home-manager/#chaoky'
 
+jump shell fish | source
 starship init fish | source
 direnv hook fish | source
