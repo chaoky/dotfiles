55a4514c03b1ec422f0562c601b51974889f1658
chaoky <levimanga@gmail.com>
2025-05-21 20:24:32 -0300

    insane rust leetcode config

--- config/nvim/lua/plugins/example.lua
+++ config/nvim/lua/plugins/example.lua
@@ -1,11 +1,46 @@
-if true then return {
-  { "wakatime/vim-wakatime" },
-	{
-    "kawre/leetcode.nvim",
-    build = ":TSUpdate html",
-    opts = {},
-	},
-} end
+if true then
+  return {
+    { "wakatime/vim-wakatime" },
+    {
+      "kawre/leetcode.nvim",
+      opts = {
+        lang = "rust",
+        injector = {
+          ["rust"] = {
+            before = { "fn main(){}", "struct Solution;" },
+          }, ---@type table<lc.lang, lc.inject>
+        },
+        hooks = {
+          ---@type fun(question: lc.ui.Question)[]
+          ["question_enter"] = {
+            function(question)
+              local problem_dir = vim.fn.stdpath("data") .. "/leetcode/Cargo.toml"
+              local content = [[
+                [package]
+                name = "leetcode"
+                edition = "2024"
+
+                [lib]
+                name = "%s"
+                path = "%s"
+              ]]
+
+              local file = io.open(problem_dir, "w")
+              if file then
+                local formatted = (content:gsub(" +", "")):format(question.q.frontend_id, question:path())
+                file:write(formatted)
+                file:close()
+              else
+                print("Failed to open file: " .. problem_dir)
+              end
+            end,
+          },
+        },
+      },
+    },
+    { "mason-org/mason.nvim", enabled = false },
+  }
+end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
