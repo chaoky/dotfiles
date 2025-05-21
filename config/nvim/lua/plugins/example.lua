4162955d5e55d1753fcc043871be084c52e18ad7
chaoky <levimanga@gmail.com>
2025-05-21 10:29:56 -0300

    couple packages from old config

--- config/nvim/lua/plugins/example.lua
+++ config/nvim/lua/plugins/example.lua
@@ -1,6 +1,11 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
-if true then return {} end
+if true then return {
+  { "wakatime/vim-wakatime" },
+	{
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    opts = {},
+	},
+} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
