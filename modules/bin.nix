823049619beca4c91fede283c1a03f37be0970bb
chaoky <levimanga@gmail.com>
2025-06-06 16:17:19 -0300

    make bin thin

--- modules/bin.nix
+++ modules/bin.nix
@@ -10,19 +10,25 @@ with lib;
 let
   cfg = config.local.bin;
   core = [
-    editorconfig-core-c bc jq gh helix gnutls binutils unzip coreutils fd
-    (ripgrep.override { withPCRE2 = true; }) texlive.combined.scheme-medium
-    pandoc fzf bat socat (aspellWithDicts ( ds: with ds; [ en en-computers en-science ]))
-    wakatime tldr cmake gcc python3 tree-sitter lazygit nodejs cargo direnv trashy
-    lua lua51Packages.luarocks-nix deno dasel pnpm prettierd stylua lsof openjdk sbt unison-ucm
-    tmux tmuxPlugins.dracula koka nixd lua-language-server rust-analyzer vtsls vscode-langservers-extracted
-    yaml-language-server vscode-js-debug terraform-ls harper dockerfile-language-server-nodejs purescript
-    purescript-language-server ffmpeg xsel xclip yt-dlp ruff basedpyright
+    editorconfig-core-c bc jq gh helix gnutls binutils
+    unzip fd (ripgrep.override { withPCRE2 = true; }) texlive.combined.scheme-medium
+    pandoc fzf bat socat coreutils wakatime tldr 
+    (aspellWithDicts ( ds: with ds; [ en en-computers en-science ])) cmake gcc 
+    python3 tree-sitter lazygit nodejs cargo direnv 
+    lua lua51Packages.luarocks-nix deno dasel pnpm prettierd 
+    stylua lsof openjdk sbt unison-ucm trashy
+    tmux tmuxPlugins.dracula koka nixd lua-language-server  vtsls
+    vscode-langservers-extracted yaml-language-server vscode-js-debug
+    terraform-ls harper dockerfile-language-server-nodejs purescript
+    purescript-language-server ffmpeg xsel xclip yt-dlp ruff
+    basedpyright rust-analyzer
   ];
   gui = [
-    krita gimp3 inkscape blender libreoffice-fresh fontforge-gtk tiled ldtk obs-studio
-    mongodb-compass dbeaver-bin redisinsight discord slack gnome-tweaks caffeine-ng ghostty
-    zen-browser.default firefox-devedition-bin stremio qbittorrent vlc bruno vscode
+    krita gimp3 inkscape blender libreoffice-fresh fontforge-gtk 
+    tiled ldtk obs-studio mongodb-compass dbeaver-bin
+    redisinsight discord slack gnome-tweaks caffeine-ng ghostty
+    zen-browser.default stremio qbittorrent vlc bruno vscode
+    zed-editor chromium via
   ];
   games = [
     gdlauncher-carbon lutris wineWowPackages.stable winetricks wineWayland
