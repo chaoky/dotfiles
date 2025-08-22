ecb9e2167f91af38674eeda6d01efae54d1c90d1
chaoky <levimanga@gmail.com>
2025-08-22 14:19:10 -0300

    relative cwd for toggle term

--- config/nvim/lua/keys.lua
+++ config/nvim/lua/keys.lua
@@ -77,95 +77,93 @@ function M.fargs(func, args)
 	return func(args)
 end
 
-
-
-
 M.keys = { -- Key discovery menu
-			"folke/which-key.nvim",
-			opts = function()
-				local tb = require("telescope.builtin")
-				local te = require("telescope").extensions
-				local fbrowser = te.file_browser.file_browser
+	"folke/which-key.nvim",
+	opts = function()
+		local tb = require("telescope.builtin")
+		local te = require("telescope").extensions
+		local fbrowser = te.file_browser.file_browser
 
-				local function format()
-					require("conform").format({ async = true, lsp_format = "fallback" })
-				end
+		local function format()
+			require("conform").format({ async = true, lsp_format = "fallback" })
+		end
 
-				return {
-					icons = { rules = false, mappings = false },
-					spec = {
-						{ "<leader>d", desc = "Document" },
-						{ "<leader>df", format, desc = "Format" },
-						{ "<leader>de", "<cmd>Telescope diagnostics<CR>", desc = "Errors" },
-						{ "<leader>dg", "<cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "Grep" },
-						{ "<leader>dd", require("dropbar.api").pick, desc = "Dropbar" },
-						{ "<leader>dr", vim.lsp.buf.rename, desc = "Rename" },
-						{ "<leader>da", vim.lsp.buf.code_action, desc = "Action" },
+		return {
+			icons = { rules = false, mappings = false },
+			spec = {
+				{ "<leader>d", desc = "Document" },
+				{ "<leader>df", format, desc = "Format" },
+				{ "<leader>de", "<cmd>Telescope diagnostics<CR>", desc = "Errors" },
+				{ "<leader>dg", "<cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "Grep" },
+				{ "<leader>dd", require("dropbar.api").pick, desc = "Dropbar" },
+				{ "<leader>dr", vim.lsp.buf.rename, desc = "Rename" },
+				{ "<leader>da", vim.lsp.buf.code_action, desc = "Action" },
 
-						{
-							"<leader>f",
-							function()
-								M.fargs(tb.find_files, { cwd = gitCwd() })
-							end,
-							desc = "Files",
-						},
-						{
-							"<leader>g",
-							function()
-								M.fargs(tb.live_grep, { cwd = gitCwd() })
-							end,
-							desc = "Grep",
-						},
-						{
-							"<leader>e",
-							function()
-								M.fargs(fbrowser, { cwd = relativeCwd() })
-							end,
-							desc = "Explore",
-						},
+				{
+					"<leader>f",
+					function()
+						M.fargs(tb.find_files, { cwd = gitCwd() })
+					end,
+					desc = "Files",
+				},
+				{
+					"<leader>g",
+					function()
+						M.fargs(tb.live_grep, { cwd = gitCwd() })
+					end,
+					desc = "Grep",
+				},
+				{
+					"<leader>e",
+					function()
+						M.fargs(fbrowser, { cwd = relativeCwd() })
+					end,
+					desc = "Explore",
+				},
 
-						{ "<leader>m", desc = "Manage" },
-						{ "<leader>mu", "<cmd>Lazy update<cr>", desc = "Update" },
-						{ "<leader>mh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
+				{ "<leader>m", desc = "Manage" },
+				{ "<leader>mu", "<cmd>Lazy update<cr>", desc = "Update" },
+				{ "<leader>mh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
 
-						{ "<leader>s", desc = "Source Control" },
-						{ "<leader>ss", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-						{ "<leader>sb", "<cmd>Gitsigns blame_line<cr>", desc = "Blame" },
+				{ "<leader>s", desc = "Source Control" },
+				{ "<leader>ss", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+				{ "<leader>sb", "<cmd>Gitsigns blame_line<cr>", desc = "Blame" },
 
-						{ "<leader>p", te.projects.projects, desc = "Projects" },
-						{ "<leader>o", te.frecency.frecency, desc = "Old" },
-						{
-							"<leader><leader>",
-							function()
-								tb.buffers({ ignore_current_buffer = true })
-							end,
-							desc = "Buffers",
-						},
-						{ "<leader>n", "<cmd>Noice dismiss<cr>", desc = "Notification" },
-						{ "<leader>k", "<cmd>bp|bd # !<cr>", desc = "Kill Buffer" },
-						{ "<leader>r", "<cmd>Telescope resume<cr>", desc = "Resume" },
+				{ "<leader>o", te.frecency.frecency, desc = "Old" },
+				{
+					"<leader><leader>",
+					function()
+						tb.buffers({ ignore_current_buffer = true })
+					end,
+					desc = "Buffers",
+				},
+				{ "<leader>n", "<cmd>Noice dismiss<cr>", desc = "Notification" },
+				{ "<leader>k", "<cmd>bp|bd # !<cr>", desc = "Kill Buffer" },
+				{ "<leader>r", "<cmd>Telescope resume<cr>", desc = "Resume" },
 
-						{ "gd", tb.lsp_definitions, desc = "Definition" },
-						{ "gr", tb.lsp_references, desc = "References" },
-						{ "gI", tb.lsp_implementations, desc = "Implementation" },
-						{ "gD", vim.lsp.buf.declaration, desc = "Declaration" },
+				{ "gd", tb.lsp_definitions, desc = "Definition" },
+				{ "gr", tb.lsp_references, desc = "References" },
+				{ "gI", tb.lsp_implementations, desc = "Implementation" },
+				{ "gD", vim.lsp.buf.declaration, desc = "Declaration" },
 
-						{ "p", "<Plug>(YankyPutAfter)", desc = "Paste" },
-						{ "P", "<Plug>(YankyPreviousEntry)", desc = "Paste Previous" },
-						{ ";", vim.diagnostic.open_float, desc = "Diagnostic" },
+				{ "p", "<Plug>(YankyPutAfter)", desc = "Paste" },
+				{ "P", "<Plug>(YankyPreviousEntry)", desc = "Paste Previous" },
+				{ ";", vim.diagnostic.open_float, desc = "Diagnostic" },
 
-						{ "U", "<cmd>Telescope undo<cr>", desc = "Undo History" },
-						{ "m", "<Cmd>e#<CR>", desc = "Move to Alternate" },
+				{ "U", "<cmd>Telescope undo<cr>", desc = "Undo History" },
+				{ "m", "<Cmd>e#<CR>", desc = "Move to Alternate" },
 
-						{
-							"<C-t>",
-							'<Cmd>execute v:count . "ToggleTerm direction=float"<CR>',
-							mode = { "n", "t" },
-							desc = "Toggle Term",
-						},
-					},
-				}
-			end,
+				{
+					"<C-t>",
+					function()
+						require("toggleterm").toggle(vim.v.count, nil, relativeCwd(), "float")
+					end,
+					mode = { "n", "t" },
+					desc = "Toggle Term",
+				},
+			},
 		}
+	end,
+}
 
 return M
