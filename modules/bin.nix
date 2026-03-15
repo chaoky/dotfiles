3e589d43e41ad6e9408c486153c0077598c7c6b6
chaoky <levimanga@gmail.com>
2026-03-15 02:20:16 -0300

    fix: handle js with fnm only

--- modules/bin.nix
+++ modules/bin.nix
@@ -5,12 +5,12 @@
         editorconfig-core-c bc jq gh helix gnutls binutils unzip fd
         (ripgrep.override { withPCRE2 = true; }) texlive.combined.scheme-medium
         pandoc fzf bat socat coreutils wakatime-cli tldr cmake gcc python3 tree-sitter
-        lazygit nodejs cargo lua lua51Packages.luarocks-nix deno dasel pnpm
-        prettierd stylua lsof openjdk sbt trashy tmux tmuxPlugins.dracula koka
+        lazygit cargo lua lua51Packages.luarocks-nix deno dasel prettierd
+        stylua lsof openjdk sbt trashy tmux tmuxPlugins.dracula koka
         nixd lua-language-server vtsls vscode-langservers-extracted yaml-language-server
         vscode-js-debug terraform-ls harper dockerfile-language-server purescript basedpyright
         purescript-language-server ffmpeg yt-dlp ruff rust-analyzer unrar psmisc gnumake
-        nix-tree claude-code github-cli jj-fzf jjui piper
+        nix-tree claude-code github-cli jj-fzf jjui piper corepack
       ];
     };
 }
